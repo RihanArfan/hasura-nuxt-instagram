e226577f3ed4e3cfd1cc335a7faf501c3be0f0fd
Rihan <me@file.properties>
2023-06-02 13:21:57 +0000

    chore: :technologist: Configure Nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  experimental: {
+    typedPages: true,
+  },
 
-})
+  typescript: {
+    strict: true,
+    tsConfig: {
+      compilerOptions: {
+        moduleResolution: "bundler",
+      },
+    },
+  },
+
+  devtools: {
+    enabled: true,
+  },
+});
