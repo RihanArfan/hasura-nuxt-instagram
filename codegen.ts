c52ad7fdfc6924e204e6f8859c74fadcfde94934
Rihan <me@file.properties>
2023-07-13 22:43:07 +0000

    feat(graphql): :sparkles: Setup codegen

---
+++ codegen.ts
@@ -0,0 +1,32 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+import { NhostClient } from "@nhost/vue";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const nhost = new NhostClient({
+  subdomain: process.env.NUXT_PUBLIC_NHOST_SUBDOMAIN,
+  region: process.env.NUXT_PUBLIC_NHOST_REGION,
+});
+
+const config: CodegenConfig = {
+  schema: {
+    [nhost.graphql.httpUrl]: {
+      headers: {
+        "x-hasura-admin-secret": process.env.NHOST_HASURA_SECRET!,
+      },
+    },
+  },
+  documents: ["**/*.vue"],
+  ignoreNoDocuments: true, // for better experience with the watcher
+  generates: {
+    "./gql/": {
+      preset: "client",
+      config: {
+        useTypeImports: true,
+      },
+    },
+  },
+};
+
+export default config;
