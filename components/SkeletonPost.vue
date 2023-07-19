b40fcfb9051349fa3a2b0bef70c28dcf6e2760f1
Rihan <me@file.properties>
2023-07-19 01:03:06 +0000

    feat(feed): :sparkles: skeleton placeholder feed

---
+++ components/SkeletonPost.vue
@@ -0,0 +1,22 @@
+<template>
+  <UCard
+    :ui="{
+      body: { padding: '' },
+      footer: { padding: 'p-2' },
+    }"
+    class="w-full"
+  >
+    <USkeleton class="pb-[100%]" />
+
+    <template #footer>
+      <div class="flex items-center gap-2">
+        <USkeleton class="h-10 w-10" :ui="{ rounded: 'rounded-full' }" />
+
+        <div class="flex flex-col gap-2">
+          <USkeleton class="h-3 w-[200px]" />
+          <USkeleton class="h-4 w-[350px]" />
+        </div>
+      </div>
+    </template>
+  </UCard>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Head>
+      <Title>Social</Title>
+    </Head>
+
+    <Body class="overscroll-x-none" />
+
+    <UContainer
+      :ui="{ constrained: 'max-w-lg' }"
+      class="flex flex-col items-center gap-3 py-2"
+    >
+      <h1 class="text-2xl font-extrabold">Social</h1>
+
+      <SkeletonPost v-for="i in 10" :key="i" />
+    </UContainer>
+  </div>
+</template>
