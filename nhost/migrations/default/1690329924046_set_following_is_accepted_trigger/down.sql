70494d0082d7e6a5a6e184eeeac43fff39e3b33f
Rihan Arfan <me@file.properties>
2023-07-26 10:13:23 +0000

    feat(graphql): :sparkles: automatically accept follow requests to public profiles

---
+++ nhost/migrations/default/1690329924046_set_following_is_accepted_trigger/down.sql
@@ -0,0 +1,19 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE OR REPLACE FUNCTION set_following_is_accepted()
+-- RETURNS TRIGGER AS $$
+-- BEGIN
+--     IF NEW.is_accepted = false AND
+--        (SELECT is_private FROM profiles WHERE id = NEW.following_profile_id) = false THEN
+--         NEW.is_accepted := true;
+--     END IF;
+--     RETURN NEW;
+-- END;
+-- $$ LANGUAGE plpgsql;
+--
+--
+--
+-- CREATE TRIGGER trigger_set_following_is_accepted
+-- BEFORE INSERT ON following
+-- FOR EACH ROW
+-- EXECUTE FUNCTION set_following_is_accepted();
---
+++ nhost/migrations/default/1690329924046_set_following_is_accepted_trigger/up.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE FUNCTION set_following_is_accepted()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.is_accepted = false AND
+       (SELECT is_private FROM profiles WHERE id = NEW.following_profile_id) = false THEN
+        NEW.is_accepted := true;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+CREATE TRIGGER trigger_set_following_is_accepted
+BEFORE INSERT ON following
+FOR EACH ROW
+EXECUTE FUNCTION set_following_is_accepted();
