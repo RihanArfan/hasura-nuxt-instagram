da8deda0c0a6742894f8a3f2185efdccdb0da4ca
Rihan Arfan <me@file.properties>
2023-07-26 10:43:52 +0000

    feat: :lipstick: match skeleton with component

--- components/SkeletonPost.vue
+++ components/SkeletonPost.vue
@@ -16,16 +16,5 @@ defineProps({
     class="w-full"
   >
     <USkeleton :ui="{ rounded: '' }" class="pb-[100%]" />
-
-    <template v-if="details" #footer>
-      <div class="flex items-center gap-2">
-        <USkeleton class="h-10 w-10" :ui="{ rounded: 'rounded-full' }" />
-
-        <div class="flex flex-col gap-2">
-          <USkeleton class="h-3 w-[200px]" />
-          <USkeleton class="h-4 w-[350px]" />
-        </div>
-      </div>
-    </template>
   </UCard>
 </template>
