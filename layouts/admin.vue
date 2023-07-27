ffbcc2ad83cccfce7cd0a20981b78f901223572f
Rihan Arfan <me@file.properties>
2023-07-27 08:40:44 +0000

    feat: :sparkles: add dark toggle and back link to nav

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -1,17 +1,34 @@
 <script setup lang="ts">
 useHead({
+  title: "Approve accounts",
   titleTemplate: (titleChunk) => {
     return titleChunk ? `${titleChunk} | Admin` : "Admin";
   },
 });
+
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
+
+const extra = computed(() => [
+  {
+    label: "Toggle dark mode",
+    icon: isDark.value ? "i-heroicons-sun" : "i-heroicons-moon",
+    click: () => toggleDark(),
+  },
+  {
+    label: "Back to Social",
+    icon: "i-heroicons-arrow-long-left-20-solid",
+    to: "/",
+  },
+]);
 </script>
 
 <template>
-  <div class="flex text-zinc-100">
-    <div class="flex h-screen bg-neutral-900 md:w-1/3">
+  <div class="flex text-zinc-900 dark:text-zinc-100">
+    <div class="flex h-screen bg-neutral-100 dark:bg-neutral-900 md:w-1/3">
       <UContainer
         :ui="{
-          base: 'ml-auto',
+          base: 'ml-auto flex flex-col',
           padding: 'py-5 px-2 lg:pl-0 pr-5',
           constrained: 'max-w-2xl',
         }"
@@ -20,7 +37,7 @@ useHead({
         <h1 class="mb-2 text-2xl">Social Administration</h1>
         <h2 class="text-xl font-bold">Pending Accounts</h2>
 
-        <div class="flex flex-col gap-3 py-4">
+        <div class="flex grow flex-col gap-3 py-4">
           <NuxtLink v-for="x in 5" :key="x" to="/">
             <UCard
               :ui="{
@@ -37,6 +54,7 @@ useHead({
             </UCard>
           </NuxtLink>
         </div>
+        <UVerticalNavigation :links="extra" />
       </UContainer>
     </div>
 
